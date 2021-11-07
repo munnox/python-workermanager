bed402da12c51d65b93ec78dfcbfa18a498c86c6
Robert Munnoch <munnox@hotmail.com>
2021-11-07 23:17:09 +0000

    WIP: Trying to clean and refactor the basic code.

--- flask_worker.py
+++ flask_worker.py
@@ -1,3 +1,12 @@
+"""Basic draft of a flask worker.
+
+Lots of TODO in this to clean up but it works currently requires further testing.
+Seems to work ok and manually testing the server from a browser.
+
+Planning to clean this up and improve a async long running process.
+
+Author: Robert Munnoch
+"""
 from multiprocessing import Process, Lock
 from typing import Any, Tuple
 from time import sleep
@@ -5,29 +14,35 @@ from datetime import datetime
 import os
 from multiprocessing import Manager
 
+
 class IFC:
     """Interface Client"""
+
     host: str
-    auth: Tuple[str,str]
-    
+    auth: Tuple[str, str]
+
     def __init__(self, host, auth):
         self.host = host
         self.auth = auth
 
+
 class Event:
     """Event"""
-    id:str
+
+    id: str
+
     def __init__(self, id):
         self.id = id
 
+
 class Context:
-    id:str
+    id: str
     ifc: IFC
     state: str = None
 
     def __init__(self, id, ifc):
         self.id = id
-        self.ifc =ifc
+        self.ifc = ifc
         self.state = 0
         # self._lock = Lock()
 
@@ -48,17 +63,16 @@ class Model:
     ifc: IFC
     state: str
 
-
     def __init__(self, name, ifc):
         self.name = name
         self.ifc = ifc
         self.state = 0
         # self._lock = Lock()
         # self.p = None
-    
+
     def setstate(self, state):
         self.state = state
-    
+
     def process(self, context):
         for i in range(60):
             context.state = context.state + 1
@@ -67,8 +81,7 @@ class Model:
             sleep(1)
 
         return context
-    
-    
+
     # def getstate(self):
     #     with self._lock:
     #         print(f"On {os.getpid()}, {id(self)} {id(self.state)} Getting state {self.state}")
@@ -87,6 +100,7 @@ class Model:
     #     # self.run(context)
     #     # self.p.join()
 
+
 class ProcessWorker(Process):
     def __init__(self, name, api, shared_dict):
         self.name = name
@@ -97,16 +111,18 @@ class ProcessWorker(Process):
     def run(self):
         context = Context("testid", self.api)
         m = Model(self.name, self.api)
+
         def setstate(state):
             print(f"set state: {state}")
-            self.shared_dict['state'] = state
+            self.shared_dict["state"] = state
+
         m.setstate = setstate
         m.process(context)
-        
 
         print(f"Shared class: {self.shared_dict}")
         # self.c.settest("other test")
         print(f"Shared class: {self.shared_dict}")
+
 
 class ProcessManager:
     worker: ProcessWorker
@@ -122,21 +138,25 @@ class ProcessManager:
     def getstate(self):
         # with self._lock:
         # print("Getting state", id(self.state), self.state)
-        print(f"On {os.getpid()}, {id(self)} {id(self.state)} Getting state from {self.state}")
+        print(
+            f"On {os.getpid()}, {id(self)} {id(self.state)} Getting state from {self.state}"
+        )
         local_state = self.state
         return local_state
 
     def setstate(self, value):
         # with self._lock:
         # print("Setting state", id(self.state), self.state)
-        print(f"On {os.getpid()}, {id(self)} {id(self.state)} Setting state from {self.state} to {id(value)} {value}")
+        print(
+            f"On {os.getpid()}, {id(self)} {id(self.state)} Setting state from {self.state} to {id(value)} {value}"
+        )
         self.state = value
-    
+
     def getstatus(self):
         status = {
             "worker": self.name,
             "alive": self.worker.is_alive(),
-            "state": self.processstate['state']
+            "state": self.processstate["state"],
         }
         status.update(dict(self.processstate))
         return status
@@ -147,7 +167,9 @@ class ProcessManager:
         self.processstate["state"] = 0
         self.processstate["context"] = context
         self.processstate["model"] = m
-        self.worker = ProcessWorker(name=self.name, api=api, shared_dict=self.processstate)
+        self.worker = ProcessWorker(
+            name=self.name, api=api, shared_dict=self.processstate
+        )
         self.worker.start()
         # self.process = Process(target=run, args=(model, context, e,d))
         # self.process.start()
@@ -162,14 +184,8 @@ def run_basic_process(name):
     pm.exec(ifc, model, Event("eventid"))
     return pm
 
-# print("main", id(model), model)
 
-# for i in range(4):
-#     print(f"Main Thread {os.getpid()} query state {pm.processstate['state']}")
-#     sleep(2)
-
-# print("query done")
-
+# TODO need to add a join for the process maybe
 # pm.worker.join()
 
 from flask import Flask, g
@@ -178,9 +194,11 @@ app = Flask("main")
 
 app_context = {}
 
+
 @app.route("/")
 def index():
     return "Server Running"
+
 
 @app.route("/start")
 @app.route("/start/<name>")
@@ -192,13 +210,14 @@ def start(name="default"):
     else:
         return f"Server not started as one exists {name}"
 
+
 @app.route("/remove")
 @app.route("/remove/<name>")
 def remove(name="default"):
     global app_context
     if name in app_context:
         status = app_context[name].getstatus()
-        if not status['alive']:
+        if not status["alive"]:
             app_context[name].worker.close()
             del app_context[name]
             return f"worker {name} removed"
@@ -217,5 +236,6 @@ def status(name="default"):
         status = app_context[name].getstatus()
 
     return f"State for {status}"
+
 
 app.run()
